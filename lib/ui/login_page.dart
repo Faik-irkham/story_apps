3c1b29435df956930135c40a5a93c046d355a3da
faik-irkham <faikirkham@gmail.com>
2024-03-11 23:04:07 +0700

    sesuaikan login page ui

--- lib/ui/login_page.dart
+++ lib/ui/login_page.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:story_apps/widgets/button_widget.dart';
 import 'package:story_apps/widgets/custom_form.dart';
 
 class LoginPage extends StatelessWidget {
@@ -7,75 +10,90 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 15),
-        children: [
-          Container(
-            height: 200,
-            margin: const EdgeInsets.only(
-              top: 100,
-              bottom: 60,
-            ),
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage(
-                  'assets/images/bg-login.png',
+      body: AnnotatedRegion(
+        value: SystemUiOverlayStyle.light,
+        child: GestureDetector(
+          child: Stack(
+            children: [
+              Container(
+                width: double.infinity,
+                height: double.infinity,
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    tileMode: TileMode.mirror,
+                    colors: [
+                      Color(0X6612111F),
+                      Color(0XCC12111F),
+                      Color(0X9912111F),
+                      Color(0XFF12111F),
+                    ],
+                  ),
                 ),
-              ),
-            ),
-          ),
-          Container(
-            height: 300,
-            padding: const EdgeInsets.all(15),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: Colors.cyanAccent,
-            ),
-            child: Column(
-              children: [
-                const SizedBox(height: 15),
-                const CustomFormField(
-                  title: 'Email',
-                  isShowTitle: false,
-                ),
-                const SizedBox(height: 15),
-                const CustomFormField(
-                  title: 'Password',
-                  isShowTitle: false,
-                  obscureText: true,
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('Login'),
-                ),
-                const SizedBox(height: 15),
-                RichText(
-                  text: const TextSpan(
-                    text: 'Don\'t have an account?',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w500,
-                    ),
+                child: SingleChildScrollView(
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 120,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      TextSpan(
-                        text: ' Sign up',
+                      const Text(
+                        'Sign In',
                         style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 14,
+                          color: Colors.white,
+                          fontSize: 40,
                           fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 50),
+                      const CustomFormField(
+                        title: 'Email',
+                        isShowTitle: false,
+                      ),
+                      const SizedBox(height: 15),
+                      const CustomFormField(
+                        title: 'Password',
+                        isShowTitle: false,
+                        obscureText: true,
+                      ),
+                      const SizedBox(height: 30),
+                      CustomFilledButton(
+                        title: 'Sign In',
+                        width: double.infinity,
+                        onPressed: () {},
+                      ),
+                      const SizedBox(height: 20),
+                      RichText(
+                        text: TextSpan(
+                          text: 'Don\'t have an account?',
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                          ),
+                          children: [
+                            TextSpan(
+                              text: ' Sign up',
+                              recognizer: TapGestureRecognizer()..onTap = () {},
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ],
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
