55e3440f1c41fe4f22bfcb662e6ffca1f480c2c3
faik-irkham <faikirkham@gmail.com>
2024-03-15 03:39:50 +0700

    add detail story page

---
+++ lib/ui/detail_story_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DetailStoryPage extends StatelessWidget {
+  const DetailStoryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
