e29285410e717efe13665ecb9d71a33622bc336b
faik-irkham <faikirkham@gmail.com>
2024-03-15 21:48:31 +0700

    sesuaikan view register

--- lib/ui/register_page.dart
+++ lib/ui/register_page.dart
@@ -1,10 +1,109 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:go_router/go_router.dart';
+import 'package:story_apps/widgets/button_widget.dart';
+import 'package:story_apps/widgets/custom_form.dart';
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: AnnotatedRegion(
+        value: SystemUiOverlayStyle.light,
+        child: GestureDetector(
+          child: Stack(
+            children: [
+              Container(
+                width: double.infinity,
+                height: double.infinity,
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    tileMode: TileMode.mirror,
+                    colors: [
+                      Color(0X6612111F),
+                      Color(0XCC12111F),
+                      Color(0X9912111F),
+                      Color(0XFF12111F),
+                    ],
+                  ),
+                ),
+                child: SingleChildScrollView(
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 120,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Register',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 50),
+                      const CustomFormField(
+                        title: 'Name',
+                        isShowTitle: false,
+                      ),
+                      const SizedBox(height: 15),
+                      const CustomFormField(
+                        title: 'Email',
+                        isShowTitle: false,
+                      ),
+                      const SizedBox(height: 15),
+                      const CustomFormField(
+                        title: 'Password',
+                        isShowTitle: false,
+                        obscureText: true,
+                      ),
+                      const SizedBox(height: 30),
+                      CustomFilledButton(
+                        title: 'Register',
+                        width: double.infinity,
+                        onPressed: () {},
+                      ),
+                      const SizedBox(height: 20),
+                      RichText(
+                        text: TextSpan(
+                          text: 'have an account?',
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                          ),
+                          children: [
+                            TextSpan(
+                              text: ' Sign In',
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  context.goNamed('login');
+                                },
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
