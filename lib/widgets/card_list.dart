38398c0fd4ac857325ec44d71013c9a7f35b15f2
faik-irkham <faikirkham@gmail.com>
2024-03-28 23:51:31 +0700

    edit card list story

--- lib/widgets/card_list.dart
+++ lib/widgets/card_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:story_apps/data/model/list_story_model.dart';
 
 class CardListStory extends StatelessWidget {
@@ -10,48 +11,56 @@ class CardListStory extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 4,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15.0),
-      ),
-      child: Column(
-        children: [
-          Container(
-            height: 280,
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(15.0),
-                topRight: Radius.circular(15.0),
-              ),
-              image: DecorationImage(
-                image: NetworkImage(stories.photoUrl!),
-                fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () {
+        context.goNamed(
+          'detail',
+          pathParameters: {'id': stories.id!},
+        );
+      },
+      child: Card(
+        elevation: 4,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        child: Column(
+          children: [
+            Container(
+              height: 280,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(15.0),
+                  topRight: Radius.circular(15.0),
+                ),
+                image: DecorationImage(
+                  image: NetworkImage(stories.photoUrl!),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 16,
-              vertical: 5,
-            ),
-            alignment: Alignment.centerLeft,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  stories.name!,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16,
+                vertical: 5,
+              ),
+              alignment: Alignment.centerLeft,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    stories.name!,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                Text(
-                  '${stories.description!.split(' ').take(10).join(' ')}...',
-                ),
-              ],
+                  Text(
+                    '${stories.description!.split(' ').take(10).join(' ')}...',
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
