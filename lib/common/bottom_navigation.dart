86e0e00de3b3e411a15be13f5583a01a586a76e9
Faik-irkham <faikirkham@gmail.com>
2024-03-31 04:06:13 +0700

    add locale for navbutton

--- lib/common/bottom_navigation.dart
+++ lib/common/bottom_navigation.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:story_apps/common/common.dart';
 import 'package:story_apps/ui/add_story_page.dart';
 import 'package:story_apps/ui/home_page.dart';
 import 'package:story_apps/ui/profile_page.dart';
@@ -13,21 +14,6 @@ class CustomBottomNavigation extends StatefulWidget {
 class _CustomBottomNavigationState extends State<CustomBottomNavigation> {
   int _bottomNavIndex = 0;
 
-  final List<BottomNavigationBarItem> _navBarItems = [
-    const BottomNavigationBarItem(
-      icon: Icon(Icons.home),
-      label: 'Home',
-    ),
-    const BottomNavigationBarItem(
-      icon: Icon(Icons.add_box_outlined),
-      label: 'Upload',
-    ),
-    const BottomNavigationBarItem(
-      icon: Icon(Icons.person),
-      label: 'Profile',
-    ),
-  ];
-
   final List<Widget> _listPage = [
     const HomePage(),
     const AddStoryPage(),
@@ -40,7 +26,20 @@ class _CustomBottomNavigationState extends State<CustomBottomNavigation> {
       body: _listPage[_bottomNavIndex],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _bottomNavIndex,
-        items: _navBarItems,
+        items: [
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.home),
+            label: AppLocalizations.of(context)!.homeNavLabel,
+          ),
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.add_box_outlined),
+            label: AppLocalizations.of(context)!.uploadNavLabel,
+          ),
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.person),
+            label: AppLocalizations.of(context)!.profileNavLabel,
+          ),
+        ],
         unselectedItemColor: Colors.grey,
         selectedIconTheme: const IconThemeData(
           color: Colors.black,
