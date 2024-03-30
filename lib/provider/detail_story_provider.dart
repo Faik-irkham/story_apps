8e95b303e010707490aace3d61879744090195df
Faik-irkham <faikirkham@gmail.com>
2024-03-30 23:48:50 +0700

    delete

--- lib/provider/detail_story_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:story_apps/data/api/api_service.dart';
-
-class DetailStoryProvider extends ChangeNotifier {
-  final ApiService apiService;
-
-  DetailStoryProvider({required this.apiService});
-}
