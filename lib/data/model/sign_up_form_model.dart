77ab6b1f41c0aca0b7613105f89d2744ec5d4356
Faik-irkham <faikirkham@gmail.com>
2024-04-03 21:11:41 +0700

    edit sign up form model to json serialization

--- lib/data/model/sign_up_form_model.dart
+++ lib/data/model/sign_up_form_model.dart
@@ -1,3 +1,7 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'sign_up_form_model.g.dart';
+
+@JsonSerializable()
 class SignUpFormModel {
   final String? name;
   final String? email;
@@ -9,11 +13,5 @@ class SignUpFormModel {
     this.password,
   });
 
-  Map<String, dynamic> toJson() {
-    return {
-      'name': name,
-      'email': email,
-      'password': password,
-    };
-  }
+  Map<String, dynamic> toJson() => _$SignUpFormModelToJson(this);
 }
